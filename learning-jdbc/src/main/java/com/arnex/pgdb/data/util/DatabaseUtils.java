ed9a2134cdaafc6f5f63000a677bbf16ef29b178
Arnold Paye <arnoldpayet@gmail.com>
2025-11-27 06:47:37 -0400

    database utils

---
+++ learning-jdbc/src/main/java/com/arnex/pgdb/data/util/DatabaseUtils.java
@@ -0,0 +1,35 @@
+package com.arnex.pgdb.data.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+public class DatabaseUtils {
+  private static final String URL = "jdbd:postgresql://localhost:5432/localdb";
+  private static final String USERNAME = "localdbuser";
+  private static final String PASSWORD = "P@ssw0rd!";
+  private static final Logger LOGGER = Logger.getLogger(DatabaseUtils.class.getName());
+  private static final String exceptionFormat = "exception in %s, message: %s, code: %s";
+  private static Connection connection;
+
+  public static Connection gConnection() {
+    if (connection == null) {
+      synchronized(DatabaseUtils.class) {
+        if (connection == null) {
+          try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+          } catch (SQLException e) {
+            handleSqlException("DatabaseUtils.getConnection", e, LOGGER);
+          }
+        }
+      }
+    }
+    return connection;
+  }
+
+  public static void handleSqlException(String method, SQLException e, Logger log) {
+    log.warning(String.format(exceptionFormat, method, e.getMessage(), e.getErrorCode()));
+    throw new RuntimeException();
+  }
+}
